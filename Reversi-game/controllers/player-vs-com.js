c7f367d49d85a8b5ae8d315da097b9e40c5b60c8
abdulmajid <majid@kebun.tech>
2022-05-24 11:18:12 +0700

    make turn move ai coin

--- Reversi-game/controllers/player-vs-com.js
+++ Reversi-game/controllers/player-vs-com.js
@@ -6,6 +6,8 @@ let boardGames;
 let initCoin;
 let turn = 1;
 let scoreLabel;
+let canMoveCoin;
+let coorSuggest = [];
 
 
 // init boards
@@ -21,6 +23,7 @@ let boards = [
 ]
 
 window.onload = function() {
+    canMoveCoin = document.getElementById("canMoveCoin");
     boardGames = document.getElementById("boardGames");
     initCoin = document.getElementById("initCoin");
     scoreLabel = document.getElementById("score");
@@ -28,6 +31,7 @@ window.onload = function() {
     boardGames.style.height = cellWidth * 8 + (gap * 9);
     drawBoardGame()
     displayCoin()
+    drawCanMoveCoin();
 }
 
 // count score
@@ -104,6 +108,44 @@ function displayCoin() {
     }
 }
 
+// draw can move coin turn
+function drawCanMoveCoin() {
+    canMoveCoin.innerHTML = "";
+    for(let row = 0; row < BOARD_LENGTH; row++) {
+        for(let column = 0; column < BOARD_LENGTH; column++) {
+            let checkValue = boards[row][column];
+            if(checkValue == 0 && canClickSpot(turn, row, column)) {
+                let discOutline = document.createElement("div");
+                discOutline.style.position = "absolute";
+                discOutline.style.width = cellWidth - 8 +'px';
+                discOutline.style.height = cellWidth - 8 +'px';
+                discOutline.style.borderRadius = "50%"
+                discOutline.style.cursor = "pointer"
+                // make a board squares by one squares
+                discOutline.style.left = (cellWidth + gap) * column + gap + 2+'px'; // column
+                discOutline.style.top = (cellWidth + gap) * row + gap + 9 +'px'; // row
+                discOutline.setAttribute("onclick", "clickedTurn("+row+", "+column+")");
+                discOutline.style.zIndex = 2;
+                if(turn == 1) {
+                    discOutline.style.border = "2px solid black"
+                }
+                if(turn == 2) {
+                    // let turnCom = document.createElement("a")
+                    // turnCom.className = "coin-suggest-com";
+                    // $('#canMoveCoin').append(
+                    //     `<a class="coin-suggest-com" href=${this}></a>`
+                    // )
+                    setTimeout(() => {
+                        discOutline.style.border = "2px solid white"
+                        computerMove(turn, row, column)
+                    }, 500)
+                }
+                canMoveCoin.appendChild(discOutline)
+            }
+        }
+    }
+}
+
 // handle clicked turn player
 function clickedTurn(row, column) {
     // console.log(row, column, 'test')
@@ -113,26 +155,89 @@ function clickedTurn(row, column) {
         return;
     }
 
-    if(canClickSpot(row, column)) {
-        let affectedBoard = getAffectedBoard(row, column);
+    if(canClickSpot(turn, row, column)) { // 
+        let affectedBoard = getAffectedBoard(turn, row, column);
         flipCoin(affectedBoard)
-        console.log(boards[row][column], 'boards')
+        // console.log(boards[row][column], 'boards')
         boards[row][column] = turn;
-        console.log(boards[row][column], 'turn')
-        if(turn == 1) {
+        // console.log(boards[row][column], 'turn')
+        if(turn == 1 && canMove(2)) {
             turn = 2
-        } else if(turn == 2) {
+        } 
+        // else if(turn == 2 && canMove(1)) {
+        //     // turn = 1
+        //     // setTimeout(() => {
+        //     //     computerMove(turn, row, column)
+        //     // }, 500)
+        // }
+    }
+    // if(turn == 2) {
+    //     setTimeout(() => {
+    //         computerMove(turn, row, column)
+    //     }, 500);
+    // }
+    // if(turn == 1) {
+    //     clickedTurn(row, column)
+    // } else if(turn == 2) {
+    //     return;
+    // }
+    // boards[row][column] = 1
+    computerMove(turn, row, column)
+    displayCoin();
+    score();
+    drawCanMoveCoin();
+}
+
+// handle computer turn
+function computerMove(turn, row, column) {
+    console.log(turn, row, column, 'coord')
+    // NOTES: COORDINAT SUGGESTED AI
+    let coorLocation = {row:row,column:column}
+    coorSuggest.push(coorLocation)
+    // console.log(coorSuggest, 'test');
+    // NOTES: RANDOM berdasarkan COORDINAT AI
+    let randomTurn = Math.floor(Math.random() * coorSuggest.length)
+    // console.log(coorSuggest[randomTurn], 'random');
+    // console.log(turn, coorSuggest[randomTurn].row, coorSuggest[randomTurn].column, 'coord random')
+    let valueAtSpot = boards[coorSuggest[randomTurn].row][coorSuggest[randomTurn].column]
+    console.log(valueAtSpot, 'valueAtSpot')
+    if(canClickSpot(turn, coorSuggest[randomTurn].row, coorSuggest[randomTurn].column) && valueAtSpot == 0) {
+        console.log(turn, coorSuggest[randomTurn].row, coorSuggest[randomTurn].column, 'coord after if')
+        let affectedBoard = getAffectedBoard(turn, coorSuggest[randomTurn].row, coorSuggest[randomTurn].column);
+        console.log(affectedBoard, 'affectedBoard')
+        flipCoin(affectedBoard);
+        if(turn == 2 && canMove(1)) {
             turn = 1
         }
+        
+        // $('.coin-suggest-com').map(function() {
+        //     coorSuggest.push($(this).attr('href'));
+        //     setTimeout(() => {
+        //         window.location.href = coorSuggest[randomTurn].row, coorSuggest[randomTurn].column
+        //     }, 500) 
+        //     
+        // })
+        // drawCanMoveCoin()
+    } else {
+        console.log("ELSE")
     }
-    // boards[row][column] = 1
-    displayCoin()
-    score();
+}
+
+// handle check canMove next turn player
+function canMove(turnId) {
+    for(let row = 0; row < BOARD_LENGTH; row++) {
+        for(let column = 0; column < BOARD_LENGTH; column++) {
+            if(canClickSpot(turnId, row, column)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 // handle player can clicked turn 
-function canClickSpot(row, column) {
-    let affectedBoard = getAffectedBoard(row, column)
+function canClickSpot(turnId, row, column) {
+    let affectedBoard = getAffectedBoard(turnId, row, column)
     // console.log(affectedBoard, 'test')
     if(affectedBoard.length == 0) {
         return false
@@ -145,6 +250,7 @@ function canClickSpot(row, column) {
 function flipCoin(affectedCoin) {
     for(let i = 0; i < affectedCoin.length; i++) {
         let spot = affectedCoin[i]
+        // console.log(spot, 'spot');
         if(boards[spot.row][spot.column] == 1) {
             boards[spot.row][spot.column] = 2
         } else {
@@ -154,7 +260,7 @@ function flipCoin(affectedCoin) {
 }
 
 // handle affected board or coin
-function getAffectedBoard(row, column) {
+function getAffectedBoard(turnId, row, column) {
     // console.log(row, column, 'test')
     let showAffectedCoin = [];
 
@@ -164,8 +270,8 @@ function getAffectedBoard(row, column) {
     while(columnIterator < BOARD_LENGTH-1) {
         columnIterator += 1;
         let valueAtSpot = boards[row][columnIterator];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffected);
             }
             break;
@@ -182,8 +288,8 @@ function getAffectedBoard(row, column) {
     while(columnIteratorLeft > 0) {
         columnIteratorLeft -= 1;
         let valueAtSpot = boards[row][columnIteratorLeft];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedLeft);
             }
             break;
@@ -199,8 +305,8 @@ function getAffectedBoard(row, column) {
     while(rowIteratorUp > 0) {
         rowIteratorUp -= 1;
         let valueAtSpot = boards[rowIteratorUp][column];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedUp);
             }
             break;
@@ -216,8 +322,8 @@ function getAffectedBoard(row, column) {
     while(rowIteratorDown < BOARD_LENGTH-1) {
         rowIteratorDown += 1;
         let valueAtSpot = boards[rowIteratorDown][column];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedDown);
             }
             break;
@@ -236,8 +342,8 @@ function getAffectedBoard(row, column) {
         rowIteratorDownRight += 1;
         columnIteratorDownRight += 1;
         let valueAtSpot = boards[rowIteratorDownRight][columnIteratorDownRight];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedDownRight);
             }
             break;
@@ -255,8 +361,8 @@ function getAffectedBoard(row, column) {
         rowIteratorDownLeft += 1;
         columnIteratorDownLeft -= 1;
         let valueAtSpot = boards[rowIteratorDownLeft][columnIteratorDownLeft];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedDownLeft);
             }
             break;
@@ -274,8 +380,8 @@ function getAffectedBoard(row, column) {
         rowIteratorUpLeft -= 1;
         columnIteratorUpLeft -= 1;
         let valueAtSpot = boards[rowIteratorUpLeft][columnIteratorUpLeft];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedUpLeft);
             }
             break;
@@ -293,8 +399,8 @@ function getAffectedBoard(row, column) {
         rowIteratorUpRight -= 1;
         columnIteratorUpRight += 1;
         let valueAtSpot = boards[rowIteratorUpRight][columnIteratorUpRight];
-        if(valueAtSpot == 0 || valueAtSpot == turn) {
-            if(valueAtSpot == turn) {
+        if(valueAtSpot == 0 || valueAtSpot == turnId) {
+            if(valueAtSpot == turnId) {
                 showAffectedCoin = showAffectedCoin.concat(couldBeAffectedUpRight);
             }
             break;
